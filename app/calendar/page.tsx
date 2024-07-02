aed58f1655be83928b4292262f2c0e88a8b1f7f1
Sanskary2303 <sanskary22@iitk.ac.in>
2024-07-02 22:25:46 +0530

    calendar

--- app/calendar/page.tsx
+++ app/calendar/page.tsx
@@ -4,76 +4,59 @@ import "../../src/css/bootstrap.css"
 import '../../src/css/agency.css';
 import '../../src/css/font-awesome/css/font-awesome.min.css';
 
-
 const Calendar: React.FC = () => {
+  const events = [
+    {
+      date: "24 August, 2024",
+      title: "Registration Date",
+      description: "The Open Source Opportunity awaits! Don't miss your chance to register for this premier event on August 24th. Mingle with fellow enthusiasts, developers, and contributors from around the world. Get ready to be inspired by thought leaders and delve into practical workshops on Snapcraft, DevOps, AI/ML, and more!",
+      image: "/img/calendar/1.jpg",
+      alt: "Registration Event"
+    },
+    {
+      date: "25 August, 2024",
+      title: "Events and Workshop",
+      description: "The 25th of August is packed with learning and engagement! Attend insightful talks, panels, and interactive sessions on cutting-edge topics like system components, cloud computing, and even OS distribution integration. Network with the open-source community and gain valuable insights to enhance your professional profile.",
+      image: "/img/calendar/2.jpg",
+      alt: "Workshop Event"
+    },
+    {
+      date: "26 August, 2024",
+      title: "Hackathon",
+      description: "Calling all coding warriors! Gear up for the 26th of August - a 12-hour hackathon designed to challenge and reward your skills. Collaborate with your team, tackle surprise challenges, and compete for amazing prizes. Enjoy complimentary food, fun activities, and the thrill of a competitive environment as you push your boundaries and create something awesome!",
+      image: "/img/calendar/3.jpg",
+      alt: "Hackathon Event"
+    }
+  ];
+
   return (
     <section id="calendar">
       <div className="container">
         <div className="row">
           <div className="col-lg-12 text-center">
             <h2 className="section-heading">Calendar</h2>
-            <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+            <h3 className="section-subheading text-muted">Upcoming Events and Workshops</h3>
           </div>
         </div>
         <div className="row">
           <div className="col-lg-12">
             <ul className="timeline">
-              <li>
-                <div className="timeline-image">
-                  <Image src="/img/calendar/1.jpg" alt="Event 1" width={400} height={400} className="img-circle" />
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>2009-2011</h4>
-                    <h4 className="subheading">Our Humble Beginnings</h4>
+              {events.map((event, index) => (
+                <li key={index} className={index % 2 === 1 ? 'timeline-inverted' : ''}>
+                  <div className="timeline-image">
+                    <Image src={event.image} alt={event.alt} width={400} height={400} className="img-circle" />
                   </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
+                  <div className="timeline-panel">
+                    <div className="timeline-heading">
+                      <h4>{event.date}</h4>
+                      <h4 className="subheading">{event.title}</h4>
+                    </div>
+                    <div className="timeline-body">
+                      <p className="text-muted">{event.description}</p>
+                    </div>
                   </div>
-                </div>
-              </li>
-              <li className="timeline-inverted">
-                <div className="timeline-image">
-                  <Image src="/img/calendar/2.jpg" alt="Event 2" width={400} height={400} className="img-circle" />
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>March 2011</h4>
-                    <h4 className="subheading">An Agency is Born</h4>
-                  </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                  </div>
-                </div>
-              </li>
-              <li>
-                <div className="timeline-image">
-                  <Image src="/img/calendar/3.jpg" alt="Event 3" width={400} height={400} className="img-circle" />
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>December 2012</h4>
-                    <h4 className="subheading">Transition to Full Service</h4>
-                  </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                  </div>
-                </div>
-              </li>
-              <li className="timeline-inverted">
-                <div className="timeline-image">
-                  <Image src="/img/calendar/4.jpg" alt="Event 4" width={400} height={400} className="img-circle" />
-                </div>
-                <div className="timeline-panel">
-                  <div className="timeline-heading">
-                    <h4>July 2014</h4>
-                    <h4 className="subheading">Phase Two Expansion</h4>
-                  </div>
-                  <div className="timeline-body">
-                    <p className="text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sunt ut voluptatum eius sapiente, totam reiciendis temporibus qui quibusdam, recusandae sit vero unde, sed, incidunt et ea quo dolore laudantium consectetur!</p>
-                  </div>
-                </div>
-              </li>
+                </li>
+              ))}
               <li className="timeline-inverted">
                 <div className="timeline-image">
                   <h4>Be Part
